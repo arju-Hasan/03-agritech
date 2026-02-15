e2b2b9b2c4090dae410c97ee9faafdf3fcef27b8
Md Rakibul Hasan Parvej <mdrakibulhasanparvej@gmail.com>
2026-02-15 14:31:33 +0600

    navbar bangla text added

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,30 @@
 import Image from "next/image";
 
 export default function Home() {
+  const links = (
+    <>
+      <li>
+        <a>Home</a>
+      </li>
+      <li>
+        <details>
+          <summary>Product</summary>
+          <ul className='p-2 bg-base-100 w-40 z-1'>
+            <li>
+              <a>সার</a>
+            </li>
+            <li>
+              <a>ধানের বীজ</a>
+            </li>
+          </ul>
+        </details>
+      </li>
+      <li>
+        <a>About</a>
+      </li>
+    </>
+  );
+
   return (
     <div>
       <div className='navbar bg-base-100 shadow-sm'>
