56227eea7c974717dae344466ec4d11d95686469
arjuhasan <rajuahmeed@gmail.com>
2026-02-15 19:09:47 +0600

    adding footer

---
+++ src/Component/Home/Hero.jsx
@@ -0,0 +1,25 @@
+
+export default function heropage() {
+    return (
+<div>
+    <div
+  className="hero min-h-screen"
+  style={{
+    backgroundImage:
+      "url(https://www.publicissapient.com/content/dam/ps-rebrand/industry/agribusiness/9-steps-to-success-for-acquiring-agritech-startups/00-AgriTechInvestment_ArticleHero_Promo1280x832.png)",
+  }}
+>
+  <div className="hero-overlay"></div>
+  <div className="hero-content text-neutral-content text-center">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold">Hello AgriTech</h1>
+      <p className="mb-5">
+        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+        quasi. In deleniti eaque aut repudiandae et a id nisi.
+      </p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+</div>
+)}
